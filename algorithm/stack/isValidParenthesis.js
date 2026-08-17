2a472033a65bf5e4174ddd4610d55c1fd49ba4a0
Khanh Dao <khanhdao@Khanhs-MacBook-Pro.local>
2026-08-17 08:58:01 -0700

    Update

---
+++ algorithm/stack/isValidParenthesis.js
@@ -0,0 +1,34 @@
+// O(n) time
+// O(n) space
+
+/**
+ * @param {string} str
+ * @return {boolean}
+ */
+export default function isValidParenthesis(str) {
+  const openingBrackets = '([{';
+  const closingBrackets = ')]}';
+  const matchingBrackets = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
+  };
+  // create a stack and pop out when we see matching open and close bracket
+  const stack = [];
+  for (const char of str) {
+    if (openingBrackets.includes(char)) {
+      stack.push(char);
+    } else if (closingBrackets.includes(char)) {
+      // Imbalance stack
+      if (stack.length === 0) {
+        return false;
+      } 
+      if (stack[stack.length - 1] === matchingBrackets[char]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
---
+++ algorithm/stack/longestParenthesis.js
@@ -0,0 +1,22 @@
+function longestValidParentheses(s) {
+  let maxLen = 0;
+  const stack = [-1];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") { // this should me format otherwise, need to check for match
+      stack.push(i);
+    } else {
+      stack.pop();
+
+      if (stack.length === 0) {
+        // This ')' cannot be matched.
+        // It becomes the new boundary.
+        stack.push(i);
+      } else {
+        maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+      }
+    }
+  }
+
+  return maxLen;
+}
